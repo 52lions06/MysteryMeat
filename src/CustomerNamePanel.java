3b156131cdb0eb75a4771721767a9c4b9ede7846
52lions06 <mckelvey.william@yahoo.com>
2018-05-05 11:56:39 -0400

    added Text field and Label to Panel Class

--- src/CustomerNamePanel.java
+++ src/CustomerNamePanel.java
@@ -2,8 +2,20 @@ import javax.swing.*;
 
 public class CustomerNamePanel extends JPanel {
 
+    private JLabel customer_name_label;
+    private JTextField customer_name_textfield;
     public CustomerNamePanel(){
+        customer_name_label = new JLabel("Enter Customer Name");
+        customer_name_label.setName("CustomerNameLabel");
+
+        customer_name_textfield = new JTextField();
+        customer_name_textfield.setName("CustomerTextField");
+
         this.setName("CustomerNamePanel");
+        this.add(customer_name_label);
+        this.add(customer_name_textfield);
 
     }
+
+
 }
--- tests/CustomerNamePanelTest.java
+++ tests/CustomerNamePanelTest.java
@@ -1,12 +1,30 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import javax.swing.*;
 
 import static org.junit.Assert.*;
 
 public class CustomerNamePanelTest {
 
+    private CustomerNamePanel panel;
+
+    @Before
+    public void setUp(){
+        panel = new CustomerNamePanel();
+
+    }
+
     @Test
     public void testNamePanelIsValid(){
+        assertEquals("CustomerNamePanel", panel.getName());
+    }
 
+    @Test
+    public void testNameLabelIsValid(){
+        JLabel customer_name_label = (JLabel) panel.getComponents()[0];
+        assertEquals("CustomerNameLabel", customer_name_label.getName());
+        assertEquals("Enter Customer Name", customer_name_label.getText());
     }
 
 }
